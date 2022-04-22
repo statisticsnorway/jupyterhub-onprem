00fdb9d60ac58734250a6fb8f86813f6c4aa59eb
Johnny Niklasson <jnk@sl-jupyterhub-t.ssb.no>
2022-04-22 15:08:45 +0200

    working AD gid and uid

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -10,11 +10,6 @@ c = get_config()
 # avoid having to rebuild the JupyterHub container every time we change a
 # configuration parameter.
 
-c.spawner.extra_container_spec = {
-    'user': '0',
-}
-
-
 # Spawn single-user servers as Docker containers
 import dockerspawner
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
@@ -64,19 +59,19 @@ c.DockerSpawner.container_image = os.environ['DOCKER_NOTEBOOK_IMAGE']
 # jupyter/docker-stacks *-notebook images as the Docker run command when
 # spawning containers.  Optionally, you can override the Docker run command
 # using the DOCKER_SPAWN_CMD environment variable.
-#spawn_cmd = os.environ.get('DOCKER_SPAWN_CMD', "start-singleuser.sh")
-#c.DockerSpawner.extra_create_kwargs.update({ 'command': spawn_cmd })
+spawn_cmd = os.environ.get('DOCKER_SPAWN_CMD', "start-singleuser.sh")
+c.DockerSpawner.extra_create_kwargs.update({ 'command': spawn_cmd, 'user': 'root'})
 
 
 #test
-c.DockerSpawner.cmd = ['jupyterhub-singleuser']
+#c.DockerSpawner.cmd = ['jupyterhub-singleuser']
 
 # Connect containers to this Docker network
 network_name = os.environ['DOCKER_NETWORK_NAME']
 c.DockerSpawner.use_internal_ip = True
 c.DockerSpawner.network_name = network_name
 # Pass the network name as argument to spawned containers
-c.DockerSpawner.extra_host_config = { 'network_mode': network_name }
+c.DockerSpawner.extra_host_config = { 'network_mode': network_name}
 
 # Memory limits
 # Documentation https://jupyterhub-dockerspawner.readthedocs.io/en/latest/api/index.html
