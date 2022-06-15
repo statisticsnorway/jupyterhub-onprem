919b462df5a0797ba3772823d8dd16d91152df41
Johnny Niklasson <jnk@ssb.no>
2022-06-15 10:19:55 +0200

    added path to  for python kernel

--- jupyterlab/kernels/python3/python.sh
+++ jupyterlab/kernels/python3/python.sh
@@ -3,5 +3,6 @@
 source /etc/profile.d/stamme_variabel
 
 export FELLES=/ssb/bruker/felles
+export PYTHONPATH=$PYTHONPATH:/ssb/bruker/felles/pythonForSsb
 
 exec /opt/conda/bin/python -m ipykernel $@
