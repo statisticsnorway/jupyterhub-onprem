3624b50f68ce38f3e57bfb837ba5ef2480ff70f0
Johnny Niklasson <jnk@ssb.no>
2023-03-07 20:04:27 +0100

    changed  to , a typo

--- docker/jupyterlab/homedir.sh
+++ docker/jupyterlab/homedir.sh
@@ -5,15 +5,15 @@
 if [ ! -d "$HOME"];
 then
     mkdir -p $HOME
-    chmod $NB_UID $HOME
+    chmod $NB_USER $HOME
 fi
 
 if [ ! -f "$HOME/.bashrc" ]; then
   cp /ssb/share/etc/skel/.bashrc "$HOME/.bashrc"
-  chown $NB_UID "$HOME/.bashrc"
+  chown $NB_USER "$HOME/.bashrc"
 fi
 
 if [ ! -f "$HOME/.bash_profile" ]; then
   cp /ssb/share/etc/skel/.bash_profile "$HOME/.bash_profile"
-  chown $NB_UID "$HOME/.bash_profile"
+  chown $NB_USER "$HOME/.bash_profile"
 fi
